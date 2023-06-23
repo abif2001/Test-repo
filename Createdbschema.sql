98d5054f262337401b272e823b5de9dfbc488c55
Abi <abigail.feeley@nationwide.co.uk>
2023-06-23 14:31:16 +0100

    data added to customers table

--- Createdbschema.sql
+++ Createdbschema.sql
@@ -5,11 +5,11 @@ USE gamesdb;
 
 CREATE TABLE customers (
 cust_id INT PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(20) NOT NULL,
-last_name VARCHAR(20),
+first_name VARCHAR(20),
+last_name VARCHAR(20) NOT NULL,
 age INT NOT NULL,
-postcode VARCHAR(10) NOT NULL,
-dob DATE NOT NULL
+postcode VARCHAR(10),
+dob DATE
 );
 
 CREATE TABLE items (
@@ -28,3 +28,13 @@ d_o_purchase DATE,
 FOREIGN KEY (item_id) REFERENCES items(item_id),
 FOREIGN KEY (cust_id) REFERENCES customers(cust_id)
 );
+
+INSERT INTO customers (cust_id, first_name, last_name, age, postcode, dob)
+VALUES (1, 'John', 'Smith', 23, 'GL50 5PS', '2000-11-17'),
+(2, 'Harry', 'Gale', 20, 'RG7 6TS', '2003-09-13');
+
+UPDATE customers
+SET first_name='new first name', last_name='new last name'
+WHERE cust_id=1;
+
+SELECT * FROM customers;
